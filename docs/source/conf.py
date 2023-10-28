b5a88d22345cb7365b1bc29a12a58cffe2d911ba
Benny Thadikaran <tbenny05@yahoo.com>
2023-10-28 18:10:46 +0530

    Version 0.2.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, src.as_posix())
 project = 'nse'
 copyright = '2023, Benny Thadikaran'
 author = 'Benny Thadikaran'
-release = '0.2.3'
+release = '0.2.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
