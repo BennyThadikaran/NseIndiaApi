acf603b0f54b375c8f2705fb0fc70ccac057491c
Benny Thadikaran <tbenny05@yahoo.com>
2024-10-14 21:49:09 +0530

    Bump to version v1.0.5

--- src/nse/NSE.py
+++ src/nse/NSE.py
@@ -29,7 +29,7 @@ class NSE:
     :raise ValueError: if ``download_folder`` is not a folder/dir
     """
 
-    __version__ = "1.0.4"
+    __version__ = "1.0.5"
     SEGMENT_EQUITY = "equities"
     SEGMENT_SME = "sme"
     SEGMENT_MF = "mf"
