2aa609fb4c05e74c24ebc69a593b57e187047295
Benny Thadikaran <tbenny05@yahoo.com>
2025-12-23 20:25:51 +0530

    Bump to version 1.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, src.as_posix())
 project = "nse"
 copyright = "2023, Benny Thadikaran"
 author = "Benny Thadikaran"
-release = "1.2.9"
+release = "1.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
